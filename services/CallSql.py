1465de4e54ee9ba9479ea43f2441797e73e0a813
jj <1246ashit@gmail.com>
2023-12-31 16:40:21 +0800

    火焰姿勢偵測更新，sql遷到CallSql裡。

--- services/CallSql.py
+++ services/CallSql.py
@@ -65,3 +65,45 @@ def SaveInSql(reportTime,names,photo_address,reportType):
     # 關閉連接
     conn.close()
     print("儲存成功")
+
+#確認 LINEID 是否有登入
+def lineUserComfirm(userId):
+    conn = pyodbc.connect(**conn_params)
+    cursor = conn.cursor()
+    sql = "SET NOCOUNT ON SELECT * FROM LineUserId WHERE userId = ?"
+    cursor.execute(sql,userId)
+    records = cursor.fetchall()
+    cursor.commit()
+    # 關閉連接
+    conn.close()
+    if len(records)!= 0:
+        print("確認為使用者")
+        return True
+    else:
+        print("查無此人")
+        return False
+
+#登入
+def registUser(userId):
+    conn = pyodbc.connect(**conn_params)
+    cursor = conn.cursor()
+    sql = "INSERT INTO LineUserId  (userId) VALUES ( ? )"
+    data=(userId)
+    cursor.execute(sql,data)
+    cursor.commit()
+    # 關閉連接
+    conn.close()
+    print("登入完成")
+    return True
+
+#取得所有使用者
+def fatchAllUser():
+    conn = pyodbc.connect(**conn_params)
+    cursor = conn.cursor()
+    sql = "SELECT userId FROM LineUserId "
+    cursor.execute(sql)
+    userIds = cursor.fetchall()
+    cursor.commit()
+    # 關閉連接
+    conn.close()
+    return userIds
--- services/YoloDerect.py
+++ services/YoloDerect.py
@@ -1,44 +1,48 @@
+import cv2
 from ultralytics import YOLO
 import math
-import cv2
-import cvzone
+#畫框框
+def drawbox(img,x1, y1, x2, y2,classNames,conf):
+    start_point = (x1, y1)  # 左上角座標 (x, y)
+    end_point = (x2, y2)  # 右下角座標 (x, y)
 
-#yolo 
-def YoloModelModelSet(frame,model,classnames):
-    result = model(frame,stream=True)
-    for info in result:
+    # 定義框框的顏色和粗細
+    color = (0, 0, 255)  # 框框顏色 (BGR)
+    thickness = 2  # 框框線條寬度
+
+    # 在框框附近繪製文字
+    text_color = (255, 255, 255)  # 白色 (BGR)
+    text_start_point = (x1, y1+20)  # 文字起始座標
+    font = cv2.FONT_HERSHEY_SIMPLEX  # 字體
+    font_scale = 0.8  # 字體縮放大小
+    end_point2=(x2, y1+20)
+    img = cv2.rectangle(img, start_point, end_point2, color, -1)
+    
+    # 在圖片上畫出框框
+    image= cv2.rectangle(img, start_point, end_point, color, thickness)
+    cv2.putText(img, f' {classNames} {conf}%', text_start_point, font, font_scale, text_color, 2)#標記
+    return image
+
+#解析資料跟畫
+def draw(results,image):
+    for info in results:
         boxes = info.boxes
         for box in boxes:
-            confidence = box.conf[0]
-            confidence = math.ceil(confidence * 100)
-            classNum = int(box.cls[0])
-            if confidence > 70:
-                x1,y1,x2,y2 = box.xyxy[0]
-                x1, y1, x2, y2 = int(x1),int(y1),int(x2),int(y2)
-                return x1, y1, x2, y2,classnames[classNum],confidence
+            #信心
+            conf=math.ceil(box.conf[0] * 100)
+            #類型
+            classNames = ["fall","fire","sit","stand"]
+            name=classNames[int(box.cls[0])]
+            #位置
+            x1,y1,x2,y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1),int(y1),int(x2),int(y2)
+            #畫
+            image=drawbox(image,x1, y1, x2, y2,name,conf)
+    return image
 
-#yolo 畫出來
-def drawAndAlert(x1,y1,x2,y2,Class,confidence,img):
-    needReport=0
-    if Class=='fire':
-        cv2.rectangle(img,(x1,y1),(x2,y2),(36, 36, 249),5)
-        cvzone.putTextRect(img, f' {Class} {confidence}%', [x1+8, y1+20],
-                                    scale=1.5,thickness=2)
-        needReport="fire"
-    return img ,needReport
-
-
-def YoloDetectAndDraw(frame,model,classNames):
-    needReport=False
-    #frame = cv2.resize(img,(640,480))
-    #偵測區
-    results = YoloModelModelSet(frame,
-                                model,
-                                classNames)
-    #畫出來
-    #
-    if results!=None:
-        results=list(results)
-        frame ,needReport=drawAndAlert(results[0],results[1],results[2],results[3],results[4],results[5],frame) 
-    
-    return frame,needReport
+#辨識
+def detect(model,image):
+    results =model.predict(
+        source=image,conf=0.7
+    )
+    return results
